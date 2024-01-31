fff3eb98724662126c7447cde175367c2ee5f2e2
Kehindebrowns <111283128+Kehindebrowns@users.noreply.github.com>
2024-01-31 16:56:26 +0100

    Update PaymentHandler.sol

--- PaymentHandler.sol
+++ PaymentHandler.sol
@@ -2,7 +2,24 @@
 pragma solidity ^0.8.0;
 
 contract PaymentHandler {
-    event PaymentReceived(address indexed from, uint256 amount);
+struct User {
+    address addr;
+    string username;
+    string passwordHash;
+
+    mapping(address => User) public users;
+
+function register(string memory _username, string memory _passwordHash) public {
+    User memory newUser = User(msg.sender, _username, _passwordHash);
+    users[msg.sender] = newUser;
+}
+function login(string memory _passwordHash) public view returns (bool) {
+    return keccak256(abi.encodePacked((users[msg.sender].passwordHash))) == keccak256(abi.encodePacked((_passwordHash)));
+}
+
+}
+  
+event PaymentReceived(address indexed from, uint256 amount);
 
     function receivePayment() external payable {
         require(msg.value > 0, "Payment must be greater than 0");
